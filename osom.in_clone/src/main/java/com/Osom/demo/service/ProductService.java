a5d3a7060e2d18e64262eb834ed6986a012ef021
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-06-20 20:39:31 +0530

    correction

--- osom.in_clone/src/main/java/com/Osom/demo/service/ProductService.java
+++ osom.in_clone/src/main/java/com/Osom/demo/service/ProductService.java
@@ -2,11 +2,15 @@ package com.Osom.demo.service;
 
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 public interface ProductService {
     public void addProductBasicDetails( String name,
                                         String description,
                                         Double price,
                                         int stock_quantity,
                                         String warehouse_location);
-    public void addProductSpecification(Double product_size , String color , String material_used, int product_id);
+    public void addProductSpecification(List<Double> product_size , String color , String material_used, int product_id);
+
+   public  void addCategory(int categoryName, int productId);
 }
